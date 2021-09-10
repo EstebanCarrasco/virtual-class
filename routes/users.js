ce2697ede3d768d10a99cbc70dcb9a10ff8b214f
Esteban Carrasco <estecarrasco@gmail.com>
2021-09-09 22:16:31 -0300

    Modifica usuarios

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken');
 
 const pool = require('../db/db');
 const auth = require('../middleware/authentication');
+const { response } = require('express');
 
 //ruta para registrar usuario
 router.post('/register',
@@ -18,9 +19,22 @@ router.post('/register',
             const id = uuidv4();
             
             //llamada bd para incertar usuario
-            const response = await pool.query('INSERT INTO usuario VALUES($1, $2, $3, $4, $5, $6, $7, $8)', [ nombre, apellido, cedula, email, contrasenaEncriptada, tipo_usuario, id, telefono ]);
+            const response = await pool.query('INSERT INTO usuario VALUES($1, $2, $3, $4, $5, $6, $7, $8)', [ nombre, apellido, cedula, email, contrasenaEncriptada, tipo_usuario, telefono, id ]);
+
+            switch (tipo_usuario) {
+                case 'Administrador':
+                    await pool.query('INSERT INTO administrador VALUES($1)', [id]);
+                    break;
+                case 'Alumno':
+                    await pool.query('INSERT INTO alumno (id, saldo_horas) VALUES($1, $2)', [id, 0]);
+                    break;
+                case 'Profesor':
+                    await pool.query('INSERT INTO profesor (id, valor_hora, materia, experiencia) VALUES($1, $2, $3, $4)', [id, 0, '', null]);
+                    break;
+            }
             res.send({results: "User created"});
         } catch (error) {
+            console.log(error)
             res.send(error);
         }
 })
@@ -34,6 +48,60 @@ router.get('/usuarios', auth,
         } catch (error) {
             res.send(error);
         }
+})
+
+router.get('/usuario/:id', auth,
+    async (req, res) => {
+        try {
+            let respuesta = {};           
+            const usuario = await pool.query('SELECT * FROM usuario WHERE id = $1',[req.params.id]);
+            if(usuario.rows[0].tipo_usuario === 'Alumno') {
+                const alumno = await pool.query('SELECT * FROM alumno WHERE id = $1',[req.params.id]);
+                respuesta = {...usuario.rows[0], ...alumno.rows[0]};
+            } else if(usuario.rows[0].tipo_usuario === 'Profesor') {
+                const profesor = await pool.query('SELECT * FROM profesor WHERE id = $1',[req.params.id]);
+                respuesta = {...usuario.rows[0], ...profesor.rows[0]};
+            } else if(usuario.rows[0].tipo_usuario === 'Administrador') {
+                respuesta = {...usuario.rows[0]};
+            }
+            res.send({usuario: respuesta});
+        } catch (error) {
+            res.send(error);
+        }
+})
+
+router.put('/administrador/:id', auth, async (req, res) => {
+    try {
+        const { nombre, apellido, email, cedula, telefono } = req.body;
+        await pool.query('UPDATE usuario SET nombre = $1, apellido = $2, email = $3, cedula = $4, telefono = $5 WHERE id = $6', [nombre, apellido, email, cedula, telefono, req.params.id]);
+        res.send('OK')
+    } catch (error) {
+        res.send(error);
+    }
+
+})
+
+router.put('/alumno/:id', auth, async (req, res) => {
+    try {
+        const { nombre, apellido, email, cedula, telefono, saldo_horas } = req.body;
+        await pool.query('UPDATE usuario SET nombre = $1, apellido = $2, email = $3, cedula = $4, telefono = $5 WHERE id = $6', [nombre, apellido, email, cedula, telefono, req.params.id]);
+        await pool.query('UPDATE alumno SET saldo_horas = $1 WHERE id = $2', [saldo_horas, req.params.id]);
+        res.send('OK')
+    } catch (error) {
+        res.send(error);
+    }
+
+})
+
+router.put('/profesor/:id', auth, async (req, res) => {
+    try {
+        const { nombre, apellido, email, cedula, telefono, valor_hora, materia, experiencia } = req.body;
+        await pool.query('UPDATE usuario SET nombre = $1, apellido = $2, email = $3, cedula = $4, telefono = $5 WHERE id = $6', [nombre, apellido, email, cedula, telefono, req.params.id]);
+        await pool.query('UPDATE profesor SET valor_hora = $1, materia = $2, experiencia = $3 WHERE id = $4', [valor_hora, materia, experiencia, req.params.id]);
+        res.send('OK')
+    } catch (error) {
+        res.send(error);
+    }
 })
 
 
